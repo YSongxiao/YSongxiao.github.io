0d684f9b3eb9fc291a3e162a22f2efe2031819b0
Songxiao Yang <48151761+BITCS0479@users.noreply.github.com>
2023-09-09 23:34:37 +0900

    Initial Commit

---
+++ vue_projs/src/components/MainPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    This is my CV
+  </div>
+  <el-button @click="show">Test</el-button>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  name: "MainPage",
+  components: {},
+  methods:{
+    show: function (){
+      console.log("You made it!");
+    }
+  }
+}
+</script>
